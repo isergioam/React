dfe31df10eaa87e27b8aae79a5691419459efcc6
Sergio Aguilar <45692292+isergioam@users.noreply.github.com>
2026-04-29 13:25:03 +0200

    chore: initialize project dependencies and configuration files

---
+++ eventos-react/src/App.jsx
@@ -0,0 +1,51 @@
+export default function App() {
+  function saludar() {
+    alert('Hola, React')
+  }
+  function iniciar() {
+    alert('La actividad comienza ahora.')
+  }
+
+  function pausar() {
+    alert('La actividad se ha pausado.')
+  }
+
+  function finalizar() {
+    alert('La actividad ha terminado.')
+  }
+
+  function elegirSala(sala) {
+    alert('Has elegido: ' + sala)
+  }
+
+  return (
+    <main>
+      <br />
+      <h1>Eventos básicos</h1>
+      <button onClick={saludar}>Saludar</button>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <h1>Panel de actividad</h1>
+      <p>Elige una acción.</p>
+
+      <button onClick={iniciar}>Iniciar</button>
+      <button onClick={pausar}>Pausar</button>
+      <button onClick={finalizar}>Finalizar</button>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <h1>Audioguía del museo</h1>
+
+      <button onClick={() => elegirSala('Sala romana')}>
+        Sala romana
+      </button>
+
+      <button onClick={() => elegirSala('Sala medieval')}>
+        Sala medieval
+      </button>
+    </main>
+  )
+}
